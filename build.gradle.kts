c1f7aef27edbb00cb68ad0e1d793899ead3fa4de
Ayfri <pierre.ayfri@gmail.com>
2022-12-22 02:09:45 +0100

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.22"
+    application
+}
+
+group = "fr.ayfri"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation("com.github.micycle1:processing-core-4:4.1.1")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
+
+application {
+    mainClass.set("MainKt")
+}
